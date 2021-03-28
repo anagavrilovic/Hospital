f93a3da3689580276c566946b7708f8042c4c82f
Stefan Ljubovic <ljubovicstefan@gmail.com>
2021-03-28 11:51:02 +0200

    .gitignore is now working

--- Hospital/Model/MedicalRecord.cs
+++ Hospital/Model/MedicalRecord.cs
@@ -1,77 +1,92 @@
+// File:    MedicalRecord.cs
+// Author:  ShowRunner
+// Created: Tuesday, March 23, 2021 10:16:14 PM
+// Purpose: Definition of Class MedicalRecord
+
 using System;
 
-namespace Hospital
+public class MedicalRecord
 {
-   public class MedicalRecord
-   {
-      public String healthCardNumber;
-      public String parentName;
-      public String address;
-      public Boolean isInsured;
-      
-      public System.Collections.Generic.List<Examination> examination;
-      
-      public System.Collections.Generic.List<Examination> Examination
-      {
-         get
-         {
-            if (examination == null)
-               examination = new System.Collections.Generic.List<Examination>();
-            return examination;
-         }
-         set
-         {
-            RemoveAllExamination();
-            if (value != null)
-            {
-               foreach (Examination oExamination in value)
-                  AddExamination(oExamination);
-            }
-         }
-      }
-      
-      
-      public void AddExamination(Examination newExamination)
-      {
-         if (newExamination == null)
-            return;
-         if (this.examination == null)
-            this.examination = new System.Collections.Generic.List<Examination>();
-         if (!this.examination.Contains(newExamination))
-         {
-            this.examination.Add(newExamination);
-            newExamination.MedicalRecord = this;
-         }
-      }
-      
-      
-      public void RemoveExamination(Examination oldExamination)
-      {
-         if (oldExamination == null)
-            return;
-         if (this.examination != null)
-            if (this.examination.Contains(oldExamination))
-            {
-               this.examination.Remove(oldExamination);
-               oldExamination.MedicalRecord = null;
-            }
-      }
-      
-      
-      public void RemoveAllExamination()
-      {
-         if (examination != null)
-         {
-            System.Collections.ArrayList tmpExamination = new System.Collections.ArrayList();
-            foreach (Examination oldExamination in examination)
-               tmpExamination.Add(oldExamination);
-            examination.Clear();
-            foreach (Examination oldExamination in tmpExamination)
-               oldExamination.MedicalRecord = null;
-            tmpExamination.Clear();
-         }
-      }
-      public Patient patient;
+   public String healthCardNumber;
+   public String parentName;
+   public Boolean isInsured;
+   public int medicalRecordID;
    
+   public System.Collections.Generic.List<Examination> examination;
+   
+   /// <summary>
+   /// Property for collection of Examination
+   /// </summary>
+   /// <pdGenerated>Default opposite class collection property</pdGenerated>
+   public System.Collections.Generic.List<Examination> Examination
+   {
+      get
+      {
+         if (examination == null)
+            examination = new System.Collections.Generic.List<Examination>();
+         return examination;
+      }
+      set
+      {
+         RemoveAllExamination();
+         if (value != null)
+         {
+            foreach (Examination oExamination in value)
+               AddExamination(oExamination);
+         }
+      }
    }
+   
+   /// <summary>
+   /// Add a new Examination in the collection
+   /// </summary>
+   /// <pdGenerated>Default Add</pdGenerated>
+   public void AddExamination(Examination newExamination)
+   {
+      if (newExamination == null)
+         return;
+      if (this.examination == null)
+         this.examination = new System.Collections.Generic.List<Examination>();
+      if (!this.examination.Contains(newExamination))
+      {
+         this.examination.Add(newExamination);
+         newExamination.MedicalRecord = this;
+      }
+   }
+   
+   /// <summary>
+   /// Remove an existing Examination from the collection
+   /// </summary>
+   /// <pdGenerated>Default Remove</pdGenerated>
+   public void RemoveExamination(Examination oldExamination)
+   {
+      if (oldExamination == null)
+         return;
+      if (this.examination != null)
+         if (this.examination.Contains(oldExamination))
+         {
+            this.examination.Remove(oldExamination);
+            oldExamination.MedicalRecord = null;
+         }
+   }
+   
+   /// <summary>
+   /// Remove all instances of Examination from the collection
+   /// </summary>
+   /// <pdGenerated>Default removeAll</pdGenerated>
+   public void RemoveAllExamination()
+   {
+      if (examination != null)
+      {
+         System.Collections.ArrayList tmpExamination = new System.Collections.ArrayList();
+         foreach (Examination oldExamination in examination)
+            tmpExamination.Add(oldExamination);
+         examination.Clear();
+         foreach (Examination oldExamination in tmpExamination)
+            oldExamination.MedicalRecord = null;
+         tmpExamination.Clear();
+      }
+   }
+   public Patient patient;
+
 }
--- Hospital/Model/Patient.cs
+++ Hospital/Model/Patient.cs
@@ -1,73 +1,88 @@
+// File:    Patient.cs
+// Author:  Marija
+// Created: Monday, March 22, 2021 2:35:05 PM
+// Purpose: Definition of Class Patient
+
 using System;
 
-namespace Hospital
+public class Patient : User
 {
-   public class Patient : User
-   {
-      public Boolean isGuest;
-      
-      public System.Collections.Generic.List<Appointment> appointment;
-      
-      public System.Collections.Generic.List<Appointment> Appointment
-      {
-         get
-         {
-            if (appointment == null)
-               appointment = new System.Collections.Generic.List<Appointment>();
-            return appointment;
-         }
-         set
-         {
-            RemoveAllAppointment();
-            if (value != null)
-            {
-               foreach (Appointment oAppointment in value)
-                  AddAppointment(oAppointment);
-            }
-         }
-      }
-      
-      
-      public void AddAppointment(Appointment newAppointment)
-      {
-         if (newAppointment == null)
-            return;
-         if (this.appointment == null)
-            this.appointment = new System.Collections.Generic.List<Appointment>();
-         if (!this.appointment.Contains(newAppointment))
-         {
-            this.appointment.Add(newAppointment);
-            newAppointment.Patient = this;
-         }
-      }
-      
-      
-      public void RemoveAppointment(Appointment oldAppointment)
-      {
-         if (oldAppointment == null)
-            return;
-         if (this.appointment != null)
-            if (this.appointment.Contains(oldAppointment))
-            {
-               this.appointment.Remove(oldAppointment);
-               oldAppointment.Patient = null;
-            }
-      }
-      
-      
-      public void RemoveAllAppointment()
-      {
-         if (appointment != null)
-         {
-            System.Collections.ArrayList tmpAppointment = new System.Collections.ArrayList();
-            foreach (Appointment oldAppointment in appointment)
-               tmpAppointment.Add(oldAppointment);
-            appointment.Clear();
-            foreach (Appointment oldAppointment in tmpAppointment)
-               oldAppointment.Patient = null;
-            tmpAppointment.Clear();
-         }
-      }
+   public Boolean isGuest;
    
+   public System.Collections.Generic.List<Appointment> appointment;
+   
+   /// <summary>
+   /// Property for collection of Appointment
+   /// </summary>
+   /// <pdGenerated>Default opposite class collection property</pdGenerated>
+   public System.Collections.Generic.List<Appointment> Appointment
+   {
+      get
+      {
+         if (appointment == null)
+            appointment = new System.Collections.Generic.List<Appointment>();
+         return appointment;
+      }
+      set
+      {
+         RemoveAllAppointment();
+         if (value != null)
+         {
+            foreach (Appointment oAppointment in value)
+               AddAppointment(oAppointment);
+         }
+      }
    }
+   
+   /// <summary>
+   /// Add a new Appointment in the collection
+   /// </summary>
+   /// <pdGenerated>Default Add</pdGenerated>
+   public void AddAppointment(Appointment newAppointment)
+   {
+      if (newAppointment == null)
+         return;
+      if (this.appointment == null)
+         this.appointment = new System.Collections.Generic.List<Appointment>();
+      if (!this.appointment.Contains(newAppointment))
+      {
+         this.appointment.Add(newAppointment);
+         newAppointment.Patient = this;
+      }
+   }
+   
+   /// <summary>
+   /// Remove an existing Appointment from the collection
+   /// </summary>
+   /// <pdGenerated>Default Remove</pdGenerated>
+   public void RemoveAppointment(Appointment oldAppointment)
+   {
+      if (oldAppointment == null)
+         return;
+      if (this.appointment != null)
+         if (this.appointment.Contains(oldAppointment))
+         {
+            this.appointment.Remove(oldAppointment);
+            oldAppointment.Patient = null;
+         }
+   }
+   
+   /// <summary>
+   /// Remove all instances of Appointment from the collection
+   /// </summary>
+   /// <pdGenerated>Default removeAll</pdGenerated>
+   public void RemoveAllAppointment()
+   {
+      if (appointment != null)
+      {
+         System.Collections.ArrayList tmpAppointment = new System.Collections.ArrayList();
+         foreach (Appointment oldAppointment in appointment)
+            tmpAppointment.Add(oldAppointment);
+         appointment.Clear();
+         foreach (Appointment oldAppointment in tmpAppointment)
+            oldAppointment.Patient = null;
+         tmpAppointment.Clear();
+      }
+   }
+
 }
