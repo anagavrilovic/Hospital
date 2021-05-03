663eb9194f078599dcb2f4afc7c9c1e091627c3c
StefanLjubovic <ljubovicstefan@gmail.com>
2021-05-03 22:09:14 +0200

    Dodata revizija leka

---
+++ Hospital/DoktorConverters/ListToStringConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Hospital.DoktorConverters
+{
+    class ListToStringConverter : IValueConverter
+    {
+        private List<string> names=new List<string>();
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            
+            foreach(Ingredient ing in (System.Collections.Generic.List<Ingredient>)value)
+            {
+                names.Add(ing.Name);
+            }
+            return String.Join(", ", ((List<string>)names).ToArray());
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Hospital/View/Doktor/OdbijeniLekoviKomentar.xaml.cs
@@ -0,0 +1,73 @@
+﻿using Hospital.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Hospital.View.Doktor
+{
+    /// <summary>
+    /// Interaction logic for OdbijeniLekoviKomentar.xaml
+    /// </summary>
+    public partial class OdbijeniLekoviKomentar : Window,INotifyPropertyChanged
+    {
+        private MedicineRevision medicineRevision = new MedicineRevision();
+        private MedicineRevisionStorage medicineRevisionStorage = new MedicineRevisionStorage();
+        private ValidnostLeka validateMedicine;
+        public event PropertyChangedEventHandler PropertyChanged;
+        public MedicineRevision MedicineRevision
+        {
+            get
+            {
+                return medicineRevision;
+            }
+            set
+            {
+                if (value != medicineRevision)
+                {
+                    medicineRevision = value;
+                    OnPropertyChanged("MedicineRevision");
+                }
+            }
+        }
+        protected void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+        public OdbijeniLekoviKomentar(MedicineRevision medicineRevision, ValidnostLeka validateMedicine)
+        {
+            InitializeComponent();
+            this.DataContext = this;
+            this.Height = (System.Windows.SystemParameters.PrimaryScreenHeight * 3 / 4);
+            this.Width = (System.Windows.SystemParameters.PrimaryScreenWidth * 1 / 2);
+            this.medicineRevision = medicineRevision;
+            this.validateMedicine = validateMedicine;
+        }
+
+        private void Nazad(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void SaveRejection_Click(object sender, RoutedEventArgs e)
+        {
+            MedicineRevision.IsMedicineRevised = true;
+            validateMedicine.MedicineRevisions.Remove(MedicineRevision);
+            medicineRevisionStorage.EditMedicine(MedicineRevision);
+            this.Close();
+        }
+    }
+}
