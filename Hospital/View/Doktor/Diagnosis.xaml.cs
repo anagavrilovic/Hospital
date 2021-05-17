2c14ed9668ad557e7889e1fd23fc812ffe41c09c
StefanLjubovic <ljubovicstefan@gmail.com>
2021-05-17 21:30:03 +0200

    Pocetak rada na MVVM semi

--- Hospital/View/Doktor/Diagnosis.xaml.cs
+++ Hospital/View/Doktor/Diagnosis.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Hospital.View.Doktor;
+using Hospital.ViewModels;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -20,64 +21,14 @@ namespace Hospital.View
 {
     
 
-    public partial class Diagnosis : Page, INotifyPropertyChanged
+    public partial class Diagnosis : Page
     {
-        private static int APPOINTMENTS_TAB = 5;
-        private static int MEDICAL_RECORD_TAB = 1;
-        private string diagnosisDescription;
-
-        public event PropertyChangedEventHandler PropertyChanged;
-        
-        public string DiagnosisDescription
-        {
-            get
-            {
-                return diagnosisDescription;
-            }
-            set
-            {
-                if (value != diagnosisDescription)
-                {
-                    diagnosisDescription = value;
-                    OnPropertyChanged("DiagnosisDescription");
-                }
-            }
-        }
-        protected void OnPropertyChanged(string name)
-        {
-            if (PropertyChanged != null)
-            {
-                PropertyChanged(this, new PropertyChangedEventArgs(name));
-            }
-        }
   
         public Diagnosis()
         {
             InitializeComponent();           
-            this.DataContext = this;
+            this.DataContext = new DiagnosisViewModel();
         }
 
-        private void Sacuvaj(object sender, RoutedEventArgs e)
-        {
-            ChangeUI();
-            ((Doctor_Examination)Window.GetWindow(this)).Examintaion.diagnosis = DiagnosisDescription;
-        }
-
-        private void ChangeUI()
-        {
-            ((Doctor_Examination)Window.GetWindow(this)).DiagnosisTab.IsEnabled = false;
-            ((Doctor_Examination)Window.GetWindow(this)).DiagnosisLabel.Foreground = Brushes.Black;
-            ConfirmBox confirmBox = new ConfirmBox("Da li je potreban termin ?");
-            if ((bool)confirmBox.ShowDialog())
-            {
-                ((Doctor_Examination)Window.GetWindow(this)).tab.SelectedIndex = APPOINTMENTS_TAB;
-                ((Doctor_Examination)Window.GetWindow(this)).AppointmentLabel.Foreground = Brushes.White;
-                ((Doctor_Examination)Window.GetWindow(this)).AppointmentTab.IsEnabled = true;
-            }
-            else
-            {
-                ((Doctor_Examination)Window.GetWindow(this)).tab.SelectedIndex = MEDICAL_RECORD_TAB;
-            }
-        }
     }
 }
---
+++ Hospital/ViewModels/DiagnosisViewModel.cs
@@ -0,0 +1,72 @@
+﻿using Hospital.Commands.DoctorCommands;
+using Hospital.View;
+using Hospital.View.Doktor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Hospital.ViewModels
+{
+    class DiagnosisViewModel : ViewModel
+    {
+        public ICommand SaveDiagnosisCommand { get; set; }
+        private static int APPOINTMENTS_TAB = 5;
+        private static int MEDICAL_RECORD_TAB = 1;
+        private string diagnosisDescription;
+        public string DiagnosisDescription
+        {
+            get
+            {
+                return diagnosisDescription;
+            }
+            set
+            {
+                if (value != diagnosisDescription)
+                {
+                    diagnosisDescription = value;
+                    OnPropertyChanged("DiagnosisDescription");
+                }
+            }
+        }
+
+        public DiagnosisViewModel()
+        {
+            SaveDiagnosisCommand = new SaveDiagnosisCommand(ExecuteMethod, canExecuteMethod); 
+        }
+
+        private bool canExecuteMethod(object parameter)
+        {
+            return true;
+        }
+        private void ExecuteMethod(object parameter)
+        {
+            MessageBox.Show("jejjj");
+           // ChangeUI();
+            //((Doctor_Examination)Window.GetWindow(this)).Examintaion.diagnosis = DiagnosisDescription;
+        }
+        /*
+        private void ChangeUI()
+        {
+            ((Doctor_Examination)Window.GetWindow(this)).DiagnosisTab.IsEnabled = false;
+            ((Doctor_Examination)Window.GetWindow(this)).DiagnosisLabel.Foreground = Brushes.Black;
+            ConfirmBox confirmBox = new ConfirmBox("Da li je potreban termin ?");
+            if ((bool)confirmBox.ShowDialog())
+            {
+                ((Doctor_Examination)Window.GetWindow(this)).tab.SelectedIndex = APPOINTMENTS_TAB;
+                ((Doctor_Examination)Window.GetWindow(this)).AppointmentLabel.Foreground = Brushes.White;
+                ((Doctor_Examination)Window.GetWindow(this)).AppointmentTab.IsEnabled = true;
+            }
+            else
+            {
+                ((Doctor_Examination)Window.GetWindow(this)).tab.SelectedIndex = MEDICAL_RECORD_TAB;
+            }
+        }
+        */
+
+    }
+}
