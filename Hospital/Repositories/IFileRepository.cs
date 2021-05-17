9c0f726c79e75db59995579d2c95d796a564a31f
Ana Gavrilovic <gavrilovic.rs65@uns.ac.rs>
2021-05-17 17:28:23 +0200

    IFileRepository interface dodat

---
+++ Hospital/Repositories/IFileRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital.Repositories
+{
+    interface IFileRepository<T>
+        where T : class
+    {
+        ObservableCollection<T> GetAll();
+        void Save(T parameter);
+        bool Delete(string id);
+        T GetByID(string id);
+        void Serialize(ObservableCollection<T> parameter);
+
+    }
+}
