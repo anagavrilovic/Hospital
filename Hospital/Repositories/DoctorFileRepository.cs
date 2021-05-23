1ed9cb1c0f04ac6deac9d424f2c55952b7dadf07
Ana Gavrilovic <gavrilovic.rs65@uns.ac.rs>
2021-05-23 12:53:58 +0200

    DoctorRepository added

---
+++ Hospital/Repositories/DoctorFileRepository.cs
@@ -0,0 +1,73 @@
+﻿using Hospital.Model;
+using Hospital.Repositories.Interfaces;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital.Repositories
+{
+    public class DoctorFileRepository : IDoctorRepository
+    {
+        private string fileName = "doctors.json";
+
+        public void Delete(string id)
+        {
+            ObservableCollection<Doctor> doctors = GetAll();
+            foreach (Doctor d in doctors)
+            {
+                if (d.PersonalID.Equals(id))
+                {
+                    doctors.Remove(d);
+                    Serialize(doctors);
+                    return;
+                }
+            }
+        }
+
+        public ObservableCollection<Doctor> GetAll()
+        {
+            ObservableCollection<Doctor> doctors;
+            using (StreamReader file = File.OpenText(@"..\\..\\Files\\" + fileName))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                doctors = (ObservableCollection<Doctor>)serializer.Deserialize(file, typeof(ObservableCollection<Doctor>));
+            }
+
+            if (doctors == null)
+                doctors = new ObservableCollection<Doctor>();
+
+            return doctors;
+        }
+
+        public Doctor GetByID(string id)
+        {
+            ObservableCollection<Doctor> doctors = GetAll();
+            foreach (Doctor d in doctors)
+                if (d.PersonalID.Equals(id))
+                    return d;
+
+            return null;
+        }
+
+        public void Save(Doctor doctor)
+        {
+            ObservableCollection<Doctor> doctors = GetAll();
+            doctors.Add(doctor);
+            Serialize(doctors);
+        }
+
+        public void Serialize(ObservableCollection<Doctor> doctors)
+        {
+            using (StreamWriter file = File.CreateText(@"..\\..\\Files\\" + fileName))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Serialize(file, doctors);
+            }
+        }
+    }
+}
--- Hospital/Services/DoctorService.cs
+++ Hospital/Services/DoctorService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Hospital.Repositories;
+using Hospital.Repositories.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,11 @@ namespace Hospital.Services
 {
     public class DoctorService
     {
+        private IDoctorRepository doctorRepository;
+
+        public DoctorService()
+        {
+            doctorRepository = new DoctorFileRepository();
+        }
     }
 }
