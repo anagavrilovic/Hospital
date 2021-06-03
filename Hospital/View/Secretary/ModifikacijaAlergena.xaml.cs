52c047679e68a5230ef63e2c09a1d19f3adeaffb
Ana Gavrilovic <gavrilovic.rs65@uns.ac.rs>
2021-06-03 03:12:19 +0200

    Drag&Drop sekretar

--- Hospital/View/Secretary/ModifikacijaAlergena.xaml.cs
+++ Hospital/View/Secretary/ModifikacijaAlergena.xaml.cs
@@ -20,6 +20,8 @@ namespace Hospital.View
 {
     public partial class ModifikacijaAlergena : Page, INotifyPropertyChanged
     {
+        Point startPoint = new Point();
+
         private MedicalRecord patientsRecord;
         public MedicalRecord PatientsRecord
         {
@@ -111,15 +113,21 @@ namespace Hospital.View
         private void BtnPlusLekoviClick(object sender, RoutedEventArgs e)
         {
             if (SelectedMedicine != null)
-                if(!PatientsRecord.Allergen.MedicineNames.Contains(SelectedMedicine))
+                if (!PatientsRecord.Allergen.MedicineNames.Contains(SelectedMedicine))
+                {
                     PatientsRecord.Allergen.MedicineNames.Add(SelectedMedicine);
+                    AllMedicines.Remove(SelectedMedicine);
+                }
         }
 
         private void BtnPlusSastojciClick(object sender, RoutedEventArgs e)
         {
             if (SelectedIngredient != null)
-                if(!PatientsRecord.Allergen.IngredientNames.Contains(SelectedIngredient))
+                if (!PatientsRecord.Allergen.IngredientNames.Contains(SelectedIngredient))
+                {
                     PatientsRecord.Allergen.IngredientNames.Add(SelectedIngredient);
+                    AllIngredients.Remove(SelectedIngredient);
+                }      
         }
 
         private void LekoviUkloni(object sender, RoutedEventArgs e)
@@ -138,6 +146,126 @@ namespace Hospital.View
             if (PropertyChanged != null)
             {
                 PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        private void ListBoxMedicinesPreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            startPoint = e.GetPosition(null);
+        }
+
+        private void ListBoxMedicinesMouseDown(object sender, MouseEventArgs e)
+        {
+            Point mousePos = e.GetPosition(null);
+            Vector diff = startPoint - mousePos;
+
+            if (e.LeftButton == MouseButtonState.Pressed &&
+                (Math.Abs(diff.X) > SystemParameters.MinimumHorizontalDragDistance ||
+                Math.Abs(diff.Y) > SystemParameters.MinimumVerticalDragDistance))
+            {
+                // Get the dragged ListViewItem
+                ListBox listBox = sender as ListBox;
+                ListBoxItem listBoxItem =
+                    FindAncestor<ListBoxItem>((DependencyObject)e.OriginalSource);
+
+                if (listBoxItem == null) return;
+
+                // Find the data behind the ListViewItem
+                string allergen = (string)listBox.ItemContainerGenerator.
+                    ItemFromContainer(listBoxItem);
+
+                // Initialize the drag & drop operation
+                DataObject dragData = new DataObject("myFormat", allergen);
+                DragDrop.DoDragDrop(listBoxItem, dragData, DragDropEffects.Move);
+            }
+        }
+
+        private static T FindAncestor<T>(DependencyObject current) where T : DependencyObject
+        {
+            do
+            {
+                if (current is T)
+                {
+                    return (T)current;
+                }
+                current = VisualTreeHelper.GetParent(current);
+            }
+            while (current != null);
+            return null;
+        }
+
+        private void ListBoxPatientsMedicinesDragOver(object sender, DragEventArgs e)
+        {
+            if (!e.Data.GetDataPresent("myFormat") || e.Source == sender)
+            {
+                e.Effects = DragDropEffects.None;
+            }
+        }
+
+        private void ListBoxPatientsMedicinesDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent("myFormat"))
+            {
+                string allergen = e.Data.GetData("myFormat") as string;
+                if (!PatientsRecord.Allergen.MedicineNames.Contains(allergen))
+                {
+                    PatientsRecord.Allergen.MedicineNames.Add(allergen);
+                    AllMedicines.Remove(allergen);
+                }
+                    
+            }
+        }
+
+        private void ListBoxIngredientsPreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            startPoint = e.GetPosition(null);
+        }
+
+        private void ListBoxIngredientsMouseDown(object sender, MouseEventArgs e)
+        {
+            Point mousePos = e.GetPosition(null);
+            Vector diff = startPoint - mousePos;
+
+            if (e.LeftButton == MouseButtonState.Pressed &&
+                (Math.Abs(diff.X) > SystemParameters.MinimumHorizontalDragDistance ||
+                Math.Abs(diff.Y) > SystemParameters.MinimumVerticalDragDistance))
+            {
+                // Get the dragged ListViewItem
+                ListBox listBox = sender as ListBox;
+                ListBoxItem listBoxItem =
+                    FindAncestor<ListBoxItem>((DependencyObject)e.OriginalSource);
+
+                if (listBoxItem == null) return;
+
+                // Find the data behind the ListViewItem
+                string allergen = (string)listBox.ItemContainerGenerator.
+                    ItemFromContainer(listBoxItem);
+
+                // Initialize the drag & drop operation
+                DataObject dragData = new DataObject("myFormat2", allergen);
+                DragDrop.DoDragDrop(listBoxItem, dragData, DragDropEffects.Move);
+            }
+        }
+
+        private void ListBoxPatientsIngredientsDragOver(object sender, DragEventArgs e)
+        {
+            if (!e.Data.GetDataPresent("myFormat2") || e.Source == sender)
+            {
+                e.Effects = DragDropEffects.None;
+            }
+        }
+
+        private void ListBoxPatientsIngredientsDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent("myFormat2"))
+            {
+                string allergen = e.Data.GetData("myFormat2") as string;
+                if (!PatientsRecord.Allergen.IngredientNames.Contains(allergen))
+                {
+                    PatientsRecord.Allergen.IngredientNames.Add(allergen);
+                    AllIngredients.Remove(allergen);
+                }
+                    
             }
         }
     }
